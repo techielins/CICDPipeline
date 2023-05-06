df5a47f8d1315ab34ae29d4d84e0dc9b1d2d8109
Linson Joseph <68058598+techielins@users.noreply.github.com>
2023-05-06 20:56:50 +0530

    Update StartApplication.java

--- jenkins-k8s/spring-boot-app/src/main/java/com/techielins/StartApplication.java
+++ jenkins-k8s/spring-boot-app/src/main/java/com/techielins/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "Placeholder page powered by  sprint boot application.V2");
+        model.addAttribute("title", "Placeholder page powered by  sprint boot application.");
         model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
         return "index";
     }
